3add4d0a32899919e73f8810437a6fff11045f07
Tara Neier <tara@productops.com>
2014-08-02 13:41:55 -0700

    enabling rest endpionts

--- flockapi/urls.py
+++ flockapi/urls.py
@@ -3,10 +3,19 @@ from django.conf.urls import patterns, include, url
 from django.contrib import admin
 admin.autodiscover()
 
+router = routers.DefaultRouter()
+router.register(r'birds', views.BirdViewSet)
+router.register(r'breeds', views.BreedViewSet)
+router.register(r'flocks', views.FlockViewSet)
+
+
 urlpatterns = patterns('',
     # Examples:
     # url(r'^$', 'flockapi.views.home', name='home'),
     # url(r'^blog/', include('blog.urls')),
 
     url(r'^admin/', include(admin.site.urls)),
+    url(r'^', include(router.urls)),
+    url(r'^api-auth/', include('rest_framework.urls', namespace='rest_framework')),
+    url(r'^docs/', include('rest_framework_swagger.urls')),
 )
