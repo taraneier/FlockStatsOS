8a47793f0e779aba3c5459c7e64a285754ea575c
Tara Neier <tara@productops.com>
2014-08-02 17:54:41 -0700

    adding import for views

--- flockapi/urls.py
+++ flockapi/urls.py
@@ -16,7 +16,7 @@ urlpatterns = patterns('',
     # url(r'^blog/', include('blog.urls')),
 
     url(r'^admin/', include(admin.site.urls)),
-    url(r'^', include(router.urls)),
+    url(r'^'api/, include(router.urls)),
     url(r'^api-auth/', include('rest_framework.urls', namespace='rest_framework')),
     url(r'^docs/', include('rest_framework_swagger.urls')),
 )
