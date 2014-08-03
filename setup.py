0cad8ddc44c08360aa88bc01a69f7e0e6e5028e3
Tara Neier <tara@productops.com>
2014-08-02 19:38:40 -0700

    adding pytz

--- setup.py
+++ setup.py
@@ -7,5 +7,5 @@ setup(
     author='Tara Neier',
     author_email='tara@neier.me',
     url='http://www.python.org/sigs/distutils-sig/',
-    install_requires=['Django>=1.6', 'Django<1.7','djangorestframework','django-rest-swagger'],
+    install_requires=['Django>=1.6', 'Django<1.7','djangorestframework','django-rest-swagger','pytz'],
 )
