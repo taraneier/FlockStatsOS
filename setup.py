cb66b5f164fe3c400e1d52422ce34a914022b45e
Tara Neier <tara@productops.com>
2014-08-04 16:40:56 -0700

    getting closer with new djangos

--- setup.py
+++ setup.py
@@ -1,10 +1,5 @@
 from setuptools import setup
 
-# Put here required packages
-packages = ['Django<=1.5',]
-
-dependency_links = ['https://www.djangoproject.com/download/1.5.1/tarball/#egg=Django-1.5.1',],
-
 setup(
     name='flockapi',
     version='0.1',
@@ -13,10 +8,10 @@ setup(
     author_email='tara@neier.me',
     url='http://www.python.org/sigs/distutils-sig/',
     install_requires=[
-        'Django>=1.5',
+        'Django>=1.6',
         'Django<1.7',
-        'django-rest-swagger',
         'djangorestframework',
+        'django-rest-swagger',
         'pytz'
     ],
 )
