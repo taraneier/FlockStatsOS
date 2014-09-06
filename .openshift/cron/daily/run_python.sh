ed407353ea7eb663d2c9d7ec52ee99b5d7de435b
Tara Neier <tara@productops.com>
2014-09-06 06:28:38 -0700

    python for cronjob learning

--- .openshift/cron/daily/run_python.sh
+++ .openshift/cron/daily/run_python.sh
@@ -2,4 +2,4 @@
 echo 'bash script is running'
 date >> ${OPENSHIFT_DATA_DIR}/ticktock.log
 source ~/python/virtenv/bin/activate
-python ${OPENSHIFT_REPO_DIR}/.openshift/cron/daily/fetch_weather.py
+python ${OPENSHIFT_REPO_DIR}/.openshift/cron/daily/fetch_weather.py >> ${OPENSHIFT_DATA_DIR}/run_python_daily.log
--- .openshift/cron/minutely/crontest.py
+++ .openshift/cron/minutely/crontest.py
@@ -5,6 +5,6 @@ import os
 __author__ = 'tneier'
 
 date = pytz.utc.localize(datetime.today()).strftime('%Y%m%d--%h%m%s')
-f = open(os.environ['OPENSHIFT_DATA_DIR'] + '/crontest.log', 'a')
+f = open(os.environ['OPENSHIFT_DATA_DIR'] + '/run_python.log', 'a')
 f.write(date)
 f.close()
