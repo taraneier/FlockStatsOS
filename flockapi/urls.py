6cc134f7c0e9fdb1f9b380c828b5521392a64757
Tara Neier <tara@productops.com>
2014-09-07 07:00:43 -0700

    adding static for angular

--- flockapi/urls.py
+++ flockapi/urls.py
@@ -2,6 +2,7 @@ from django.conf.urls import patterns, include, url
 from rest_framework import routers
 from birds import views
 from django.contrib import admin
+from django.conf import settings
 admin.autodiscover()
 
 router = routers.DefaultRouter()
@@ -20,5 +21,5 @@ urlpatterns = patterns('',
     url(r'^api/', include(router.urls)),
     url(r'^api-auth/', include('rest_framework.urls', namespace='rest_framework')),
     url(r'^docs/', include('rest_framework_swagger.urls')),
-
+    url(r'^static/', settings.STATIC_ROOT)
 )
