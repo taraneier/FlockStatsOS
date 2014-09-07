d4005eb9ddfcca23eb8160c02838bba2062abdc2
Tara Neier <tara@productops.com>
2014-09-07 06:03:56 -0700

    initial ng workings, template trouble

--- birds/views.py
+++ birds/views.py
@@ -1,10 +1,15 @@
 from models import Bird, Breed, Flock
 from rest_framework import viewsets
+from django.shortcuts import render
 from birds.serializers import BirdSerializer, BreedSerializer, FlockSerializer
+
+def index(request):
+    return render(request, 'index.html')
+
 
 class FlockViewSet(viewsets.ReadOnlyModelViewSet):
     """
-    API endpoint that allows birds to be viewed or edited.
+    API endpoint that allows flocks to be viewed.
     """
     queryset = Flock.objects.all()
     serializer_class = FlockSerializer
---
+++ wsgi/static/ng/app.js
@@ -0,0 +1,4 @@
+/**
+ * Created by tneier on 9/6/14.
+ */
+angular.module('FlockApp',[]);
