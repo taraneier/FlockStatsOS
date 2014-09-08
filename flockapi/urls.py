e3a5a1465d4f68fc88414aa1025e4a45785f4ed3
Tara Neier <tara@productops.com>
2014-09-07 18:17:49 -0700

    static files working

--- flockapi/urls.py
+++ flockapi/urls.py
@@ -3,6 +3,8 @@ from rest_framework import routers
 from birds import views
 from django.contrib import admin
 from django.conf import settings
+from django.conf.urls.static import static
+
 admin.autodiscover()
 
 router = routers.DefaultRouter()
@@ -21,5 +23,4 @@ urlpatterns = patterns('',
     url(r'^api/', include(router.urls)),
     url(r'^api-auth/', include('rest_framework.urls', namespace='rest_framework')),
     url(r'^docs/', include('rest_framework_swagger.urls')),
-    url(r'^static/', settings.STATIC_ROOT)
 )
