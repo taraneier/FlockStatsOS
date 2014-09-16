94271ae8a86257d8961939d022861ceded4685d4
Tara Neier <tara@productops.com>
2014-09-15 19:51:52 -0700

    weather incorporated, 2nd db, sub urls

---
+++ weather/urls.py
@@ -0,0 +1,9 @@
+__author__ = 'tneier'
+from django.conf.urls import patterns, include, url
+
+urlpatterns = patterns('weather.views',
+    ('daily/$', 'daily'),
+    ('daily/(?P<days>\d{3})/$', 'dailynum'),
+    ('daily/(?P<days>\d{2})/$', 'dailynum'),
+    ('daily/(?P<days>\d{1})/$', 'dailynum'),
+)
