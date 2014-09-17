df30882e5624e9ddbbc8f3f6ef422d64ee2e0767
Tara Neier <tara@productops.com>
2014-09-17 08:59:03 -0700

    cleaned up flock api to remove things handled elsewhere

--- birds/serializers.py
+++ birds/serializers.py
@@ -1,24 +1,24 @@
 from models import Bird, Breed, Flock
 from rest_framework import serializers
 
+
 class FlockSerializer(serializers.HyperlinkedModelSerializer):
     bird_count = serializers.Field()
     egg_count = serializers.Field()
     avg_weight = serializers.Field()
     days_laying = serializers.Field()
     eggs_per_day = serializers.Field()
-    favorite_sites = serializers.Field()
-    top_layers = serializers.Field()
-    last_week = serializers.Field()
+
     class Meta:
         model = Flock
-        fields = ('flock_id','name','bird_count','egg_count','avg_weight','days_laying','eggs_per_day','favorite_sites', 'top_layers','last_week')
+        fields = ('flock_id', 'name', 'bird_count', 'egg_count', 'avg_weight', 'days_laying', 'eggs_per_day')
 
 
 class BreedSerializer(serializers.HyperlinkedModelSerializer):
     class Meta:
         model = Breed
-        fields = ('breed_id','name', 'origin')
+        fields = ('breed_id', 'name', 'origin')
+
 
 class BirdSerializer(serializers.HyperlinkedModelSerializer):
     age = serializers.Field()
@@ -28,6 +28,9 @@ class BirdSerializer(serializers.HyperlinkedModelSerializer):
     days_laying = serializers.Field()
     eggs_per_day = serializers.Field()
     favorite_site = serializers.Field()
+
     class Meta:
         model = Bird
-        fields = ('bird_id','name', 'breed', 'hatched','age', 'egg_count', 'avg_weight', 'days_laying', 'eggs_per_day', 'favorite_site')
+        fields = (
+        'bird_id', 'name', 'breed', 'hatched', 'age', 'egg_count', 'avg_weight', 'days_laying', 'eggs_per_day',
+        'favorite_site')
