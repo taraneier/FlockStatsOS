8ca12f1fcde776d600c90759fba555bc54262dae
Tara Neier <tara@productops.com>
2014-09-24 19:40:12 -0700

    attempt at auto dates for egg add

--- birds/static/js/admin.js
+++ birds/static/js/admin.js
@@ -4,18 +4,19 @@
 
 django.jQuery(document).ready(function() {
 //
-    dateVal = window.location.search.replace('?', '')
-    if (dateVal.length < 10) {
-        dateVal = new Date().toLocaleDateString();
-    }
-
-
-    django.jQuery('#id_start_0').val(dateVal);
-    django.jQuery('#id_finish_0').val(dateVal);
-
-//    query = window.location.search.replace('?', '')
-//    if (query == 'x') {
-//        DateTimeShortcuts.handleCalendarQuickLink(0, 0);
-//        DateTimeShortcuts.handleCalendarQuickLink(1, 0);
+//    dateVal = window.location.search.replace('?', '')
+//    if (dateVal.length < 10) {
+//        dateVal = new Date().toLocaleDateString();
 //    }
+//
+//
+//    django.jQuery('#id_start_0').val(dateVal);
+//    django.jQuery('#id_finish_0').val(dateVal);
+
+    query = window.location.search.replace('?', '')
+    if (query == 'x') {
+        DateTimeShortcuts.init();
+        DateTimeShortcuts.handleCalendarQuickLink(0, 0);
+        DateTimeShortcuts.handleCalendarQuickLink(1, 0);
+    }
 });
