6417f67776890e14ac2e062456bfaa250a647873
Tara Neier <tara@productops.com>
2014-10-26 19:40:56 -0700

    removing minutely jobs

--- .openshift/cron/minutely/test.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-echo 'bash script is running'
-#date >> ${OPENSHIFT_DATA_DIR}/ticktock.log
-#source ~/python/virtenv/bin/activate
-#python ${OPENSHIFT_REPO_DIR}/.openshift/cron/minutely/crontest.py
