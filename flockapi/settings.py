334518f07ce80606eff0fbbb85536ff49421a8dc
Tara Neier <tara@productops.com>
2014-11-16 17:41:11 -0800

    tweaking media_url

--- flockapi/settings.py
+++ flockapi/settings.py
@@ -172,4 +172,4 @@ else:
 # URL that handles the media served from MEDIA_ROOT. Make sure to use a
 # trailing slash.
 # Examples: "http://media.lawrence.com/media/", "http://example.com/media/"
-MEDIA_URL = '/media/'
+MEDIA_URL = '/static/media/'
