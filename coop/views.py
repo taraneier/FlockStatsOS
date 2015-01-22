b677dd119d84d85b0ce012eece1a1978b917ccc4
Tara Neier <tara@neier.org>
2015-01-21 18:25:16 -0800

    changing for dash display

--- coop/views.py
+++ coop/views.py
@@ -15,7 +15,7 @@ def upload(request, hum, t0, t1, t2, t3, t4, t5, t6, lum1, lum2, door):
 
 
 def door(request):
-    response_data = {"value": 0, "formatted" : "Open"}
+    response_data = "Open"
     return HttpResponse(json.dumps(response_data), content_type="application/json")
 
 def lum1(request):
